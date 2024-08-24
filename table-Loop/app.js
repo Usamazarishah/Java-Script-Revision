b37eea1ee32026b79e11423f853ef3533d92d822
M.Usama <usamazarishah@gmail.com>
2024-08-24 09:49:23 +0500

    Multiplication-table

---
+++ table-Loop/app.js
@@ -0,0 +1,6 @@
+var number  =  prompt("Enter Your Number") ;
+for (var i=1; i<=10; i++){
+    document.getElementById("multiplication-table").innerHTML +=
+    `${number} * ${i} = ${number*i}<br>`;
+
+}
