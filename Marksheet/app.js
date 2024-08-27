b251b338e5575a72926b1efc698898e58ea2385a
M.Usama <usamazarishah@gmail.com>
2024-08-27 09:36:12 +0500

    Marksheet done

---
+++ Marksheet/app.js
@@ -0,0 +1,105 @@
+var urdu = +prompt("Enter Your Urdu Number ");
+var english  = +prompt("Enter Your English Number"); 
+var islamiyat = +prompt("Enter Your Islamiyat Number");
+var computer_science = +prompt("Enter Your Computer Science Number");
+var physics = +prompt ("Enter Your Physics Number");
+var math = +prompt ("Enter Your Math Number");
+
+var secured = parseInt((urdu + english + islamiyat + computer_science + physics + math));
+var percentage = (urdu + english + islamiyat + computer_science + physics + math)/550*100;
+var grade_num = parseInt((urdu + english + islamiyat + computer_science + physics + math));
+
+
+var urdu_number = document.querySelector(".urdu_number").innerHTML += urdu;
+var english_number = document.querySelector(".english_number").innerHTML += english;
+var islamiyat_number = document.querySelector(".islamiyat_number").innerHTML += islamiyat;
+var computer_number = document.querySelector(".computer_number").innerHTML += computer_science;
+var physics_number = document.querySelector(".physics_number").innerHTML += physics;
+var math_number = document.querySelector(".math_number").innerHTML += math;
+
+var urdu_number_1 = document.querySelector(".urdu_number_1").innerHTML  += urdu;
+var english_number_1 = document.querySelector(".english_number_1").innerHTML  += english;
+var islamiyat_number_1 = document.querySelector(".islamiyat_number_1").innerHTML += islamiyat;
+var computer_number_1 = document.querySelector(".computer_number_1").innerHTML += computer_science;
+var physics_number_1 = document.querySelector(".physics_number_1").innerHTML += physics;
+var math_number_1 = document.querySelector(".math_number_1").innerHTML += math;
+
+var secured_number = document.querySelector(".secured_number").innerHTML += secured;
+var percentage_number = document.querySelector(".percentage_number").innerHTML +=  `${percentage}%`;
+
+
+var grade;
+
+if (grade_num >= 440 ) {
+    grade = `A1`;
+}
+else if (grade_num >=385){
+    grade = `A`;
+}
+else if (grade_num >= 330){
+    grade = `B`;
+}
+else if (grade_num >= 275){
+    grade = `C`;
+}
+else if (grade_num >= 220) {
+    grade = `D`;
+}
+else{
+    grade = `F`;
+}
+document.querySelector(".grade").innerHTML = grade; 
+
+var urdu_remarks; 
+if (urdu >= 40){
+    urdu_remarks = `Pass`;
+}
+else{
+    urdu_remarks = `Fail`;
+}
+document.querySelector(".urdu_remarks").innerHTML = urdu_remarks;
+
+var english_remarks;
+if (english >= 40){
+    english_remarks = `Pass`;  
+}
+else{
+    english_remarks = `Fail`;
+}
+document.querySelector(".english_remarks").innerHTML = english_remarks;
+
+var islamiyat_remarks;
+if (islamiyat >= 20){
+    islamiyat_remarks = `Pass`;
+}
+else{
+    islamiyat_remarks = `Fail`;
+}
+document.querySelector(".islamiyat_remarks").innerHTML = islamiyat_remarks;
+
+var computer_remarks;
+if (computer_science >= 40){
+    computer_remarks = `Pass`;
+}
+else{
+    computer_remarks = `Fail`;
+}
+document.querySelector(".computer_remarks").innerHTML = computer_remarks;
+
+var physics_remarks;
+if (physics >= 40){
+    physics_remarks = `Pass`;
+}
+else{
+    physics_remarks = `Fail`;
+}
+document.querySelector(".physics_remarks").innerHTML = physics_remarks;
+
+var  math_remarks; 
+if (math >= 40){
+    math_remarks = `Pass`;
+}
+else{
+    math_remarks = `Fail`;
+}
+document.querySelector(".math_remarks").innerHTML = math_remarks;
