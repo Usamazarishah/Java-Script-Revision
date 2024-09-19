1ebb04e33200a11c3368cbeb2a91876dcf854596
M.Usama <usamazarishah@gmail.com>
2024-09-19 08:50:24 +0500

    Loops

---
+++ Loops/app.js
@@ -0,0 +1,43 @@
+var array =  [
+    [5, 4, 2],
+    [9, 2, 10],
+    [7, 15, 7], 
+    [7, 15, 7], 
+  ]
+
+console.log(array);
+for(var i=0; i<array.length; i++){
+    for(var j=0; j<array[i].length; j++){
+        console.log(array[i][j]);
+    }
+}
+
+// const matrix1 = [
+//     [3, 2, 5],
+//     [5, 4, 6],
+//     [1, 0, 90],
+//   ];
+//   const matrix2 = [
+//     [7, 2, 1],
+//     [5, 9, 2],
+//     [1, 0, 19],
+//   ];
+//   var result=[] ;  
+//   for (let i = 0; i < 3; i++) {
+//     var array = []
+//     for (let j = 0; j < 3; j++) {
+//       // console.log(matrix1[i][j] + matrix2[i][j]);
+//       let plus = matrix1[i][j] + matrix2[i][j]
+//       array.push(plus)
+//     }
+//     result.push(array)
+
+//   }    console.log(result)
+  
+  
+// ye chiye
+// result = [
+//   [10, 4, 5],
+//   [10, 13, 8],
+//   [2, 0, 109],
+// ];;
