9a1a36cbf35c9201bf8d3cc3a0f45a7a18507fda
M.Usama <usamazarishah@gmail.com>
2024-09-23 14:35:32 +0500

    table-Loop_done

--- table-Loop/app.js
+++ table-Loop/app.js
@@ -1,7 +1,7 @@
-// var number  =  prompt("Enter Your Number") ;
-// for (var i=1; i<=10; i++){
-//     document.getElementById("multiplication-table").innerHTML +=
-//     `${number} * ${i} = ${number*i}<br>`;
+var number  =  prompt("Enter Your Number") ;
+for (var i=1; i<=10; i++){
+    document.getElementById("multiplication-table").innerHTML +=
+    `${number} * ${i} = ${number*i}<br>`;
 
-// }
+}
 
