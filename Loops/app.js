edf6d9263cac53b8b492e2fd0c37cdb96af76353
M.Usama <usamazarishah@gmail.com>
2024-09-23 14:35:51 +0500

    Loops_done

--- Loops/app.js
+++ Loops/app.js
@@ -12,32 +12,31 @@ for(var i=0; i<array.length; i++){
     }
 }
 
-// const matrix1 = [
-//     [3, 2, 5],
-//     [5, 4, 6],
-//     [1, 0, 90],
-//   ];
-//   const matrix2 = [
-//     [7, 2, 1],
-//     [5, 9, 2],
-//     [1, 0, 19],
-//   ];
-//   var result=[] ;  
-//   for (let i = 0; i < 3; i++) {
-//     var array = []
-//     for (let j = 0; j < 3; j++) {
-//       // console.log(matrix1[i][j] + matrix2[i][j]);
-//       let plus = matrix1[i][j] + matrix2[i][j]
-//       array.push(plus)
-//     }
-//     result.push(array)
+const matrix1 = [
+    [3, 2, 5],
+    [5, 4, 6],
+    [1, 0, 90],
+  ];
+  const matrix2 = [
+    [7, 2, 1],
+    [5, 9, 2],
+    [1, 0, 19],
+  ];
+  var result=[] ;  
+  for (let i = 0; i < 3; i++) {
+    var array = []
+    for (let j = 0; j < 3; j++) {
+      // console.log(matrix1[i][j] + matrix2[i][j]);
+      let plus = matrix1[i][j] + matrix2[i][j]
+      array.push(plus)
+    }
+    result.push(array)
 
-//   }    console.log(result)
+  }    console.log(result)
   
-  
-// ye chiye
-// result = [
-//   [10, 4, 5],
-//   [10, 13, 8],
-//   [2, 0, 109],
-// ];;
+//ye chiye
+result = [
+  [10, 4, 5],
+  [10, 13, 8],
+  [2, 0, 109],
+];;
